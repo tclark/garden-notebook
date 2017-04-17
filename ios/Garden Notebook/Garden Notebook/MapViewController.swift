260e464814bac15aa7c1104c8c2f6b1aaef58950
Tom Clark <tom@runawayplay.com>
2017-04-17 18:00:20 +1200

    added iOS project, initial storyboards

---
+++ ios/Garden Notebook/Garden Notebook/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Garden Notebook
+//
+//  Created by Tom on 17/04/17.
+//  Copyright © 2017 Tom. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios/Garden Notebook/Garden Notebook/NotesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Garden Notebook
+//
+//  Created by Tom on 17/04/17.
+//  Copyright © 2017 Tom Clark. 
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
